590c40380483b7c462ee2ac5dcc7312890cbedb2
Brandy Kundenga <55144997+BrandyKun@users.noreply.github.com>
2024-08-26 02:23:09 +0100

    added db set context items

--- src/Infrastructure/Context/ContentDbContext.cs
+++ src/Infrastructure/Context/ContentDbContext.cs
@@ -13,4 +13,6 @@ public class ContentDbContext : DbContext
     public DbSet<Page> Pages { get; set; }
     public DbSet<Element> Elements { get; set; }
     public DbSet<Template> Templates { get; set; }
+    public DbSet<Menu> Menus { get; set; }
+    public DbSet<MenuItem> MenuItems { get; set; }
 }
