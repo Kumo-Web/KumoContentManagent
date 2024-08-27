6c8280213202a98fa0aa003444eb06a263d52e40
Brandy Kundenga <55144997+BrandyKun@users.noreply.github.com>
2024-08-27 10:05:31 +0100

    added to context

--- src/Infrastructure/Context/ContentDbContext.cs
+++ src/Infrastructure/Context/ContentDbContext.cs
@@ -13,4 +13,6 @@ public class ContentDbContext : DbContext
     public DbSet<Page> Pages { get; set; }
     public DbSet<Element> Elements { get; set; }
     public DbSet<Template> Templates { get; set; }
+    public DbSet<Form> Forms { get; set; }
+    public DbSet<FormSubmission> FormSubmissions { get; set; }
 }
